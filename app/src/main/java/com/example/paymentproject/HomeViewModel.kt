f288b06d42ee65135b8f32fccab4fc9276315877
pleavinseven <pleavinseven@gmail.com>
2022-05-08 21:50:13 +0200

    - checkEmailValid function created with regex

--- app/src/main/java/com/example/paymentproject/HomeViewModel.kt
+++ app/src/main/java/com/example/paymentproject/HomeViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.paymentproject
 
+import java.util.regex.Pattern
+
 class HomeViewModel {
 
-    fun checkCredentials() {
-        TODO("Not yet implemented")
+
+    private val emailRegex =  "^(.+)@(.+)\$"
+
+    fun checkEmailValid(email: String):Boolean {
+        return Pattern.matches(emailRegex, email)
     }
 
+    fun emailErrorMessage(email: String):String{
+        return "Email $email is invalid"
+    }
 
 }
