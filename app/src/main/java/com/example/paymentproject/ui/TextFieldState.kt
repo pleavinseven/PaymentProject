60371128b27fae2f476223c38e29ab907fa43429
pleavinseven <pleavinseven@gmail.com>
2022-05-14 23:13:26 +0200

    initial commit

---
+++ app/src/main/java/com/example/paymentproject/ui/TextFieldState.kt
@@ -0,0 +1,23 @@
+package com.example.paymentproject.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+open class TextFieldState (
+    private val validator: (String) -> Boolean = { true },
+    private val errorMessage: (String) -> String
+
+){
+    var text by mutableStateOf("")
+    var error by mutableStateOf<String?>(null)
+
+    fun validate(){
+        error = if(validator(text)){
+            null
+        } else{
+            errorMessage(text)
+        }
+    }
+
+}
