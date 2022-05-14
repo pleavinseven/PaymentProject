1d04d55dc5167185f13482ca115fb30b17723754
pleavinseven <pleavinseven@gmail.com>
2022-05-14 23:13:54 +0200

    moved contents to EmailState class

--- app/src/main/java/com/example/paymentproject/HomeViewModel.kt
+++ app/src/main/java/com/example/paymentproject/HomeViewModel.kt
@@ -1,18 +1,7 @@
 package com.example.paymentproject
 
-import java.util.regex.Pattern
-
 class HomeViewModel {
 
 
-    private val emailRegex =  "^(.+)@(.+)\$"
-
-    fun checkEmailValid(email: String):Boolean {
-        return Pattern.matches(emailRegex, email)
-    }
-
-    fun emailErrorMessage(email: String):String{
-        return "Email $email is invalid"
-    }
 
 }
