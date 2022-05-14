53b0d8997bba093cf94ba55904a757cafcddfc47
pleavinseven <pleavinseven@gmail.com>
2022-05-14 23:13:10 +0200

    initial commit

---
+++ app/src/main/java/com/example/paymentproject/ui/EmailState.kt
@@ -0,0 +1,18 @@
+package com.example.paymentproject.ui
+
+import android.text.TextUtils
+import android.util.Patterns
+
+class EmailState: TextFieldState(
+    validator = ::checkEmailValid,
+    errorMessage = ::emailErrorMessage
+) {
+}
+
+fun checkEmailValid(email: String):Boolean {
+    return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+}
+
+fun emailErrorMessage(email: String):String{
+    return "Email $email is invalid"
+}
