1fef8fc0587e2a9f5c3ea4db9102117cf7fedf90
pleavinseven <pleavinseven@gmail.com>
2022-07-12 01:38:34 +0200

    password regex added

--- app/src/main/java/com/example/paymentproject/ui/model/PasswordState.kt
+++ app/src/main/java/com/example/paymentproject/ui/model/PasswordState.kt
@@ -3,11 +3,15 @@ package com.example.paymentproject.ui.model
 class PasswordState: TextFieldState(
     validator = ::checkPasswordValid,
     errorMessage = {passwordErrorMessage()}
-) {
-}
+)
 
 fun checkPasswordValid(password: String):Boolean {
-    return true // database call goes here
+    password.let {
+        val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$"
+        val passwordMatcher = Regex(passwordPattern)
+
+        return passwordMatcher.find(password) != null
+    }
 }
 
-fun passwordErrorMessage() = "password incorrect"
+fun passwordErrorMessage() = "password must contain upper and lowercase letters, at least one number and a minimum of 8 characters"
